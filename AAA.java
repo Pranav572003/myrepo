3f82674777db654044d41a397604d686b11bf6ff
Pranav572003 <pranavjadhav0503@gmail.com>
2025-07-18 23:54:09 +0530

    Update AAA.java

--- AAA.java
+++ AAA.java
@@ -2,7 +2,7 @@ public class AAA{
   public static void main(String x[]){
     int qty=Integer.parseInt(x[0]);
 	int rate = Integer.parseInt(x[1]);
-	
+	int ra = Integer.parseInt(x[2]);
 	int total=qty*rate;
 	int gstamt=total*18/100;
 	total=total+gstamt;
