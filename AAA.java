451e912f4ad412ede7bc1b038a1fc33c6949e9d3
Pranav572003 <pranavjadhav0503@gmail.com>
2025-07-18 23:20:58 +0530

    AAA

---
+++ AAA.java
@@ -0,0 +1,12 @@
+public class AAA{
+  public static void main(String x[]){
+    int qty=Integer.parseInt(x[0]);
+	int rate = Integer.parseInt(x[1]);
+	
+	int total=qty*rate;
+	int gstamt=total*18/100;
+	total=total+gstamt;
+	System.out.println(total);
+	
+  }
+}
